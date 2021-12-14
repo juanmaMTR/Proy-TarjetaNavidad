471e83aa54b49510190ef56c602bb85da7349433
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-12-14 19:18:59 +0100

    JS en proceso..

---
+++ js/crear.js
@@ -0,0 +1,50 @@
+/**
+ * @file crear.js
+ * Archivo de javascript para crear la tarjeta de navidad
+ * @author Juan Manuel Toscano Reyes <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
+ * @license GPL V.3 2021
+ */
+'use strict'
+
+window.onload=iniciar
+/**
+ * Función que se ejecuta cuando se carga window
+ */
+function iniciar(){
+    let valor=leerCookie()
+    console.log(valor);
+    if(valor=='eleccion1'){
+        let frase='Tengo un sueño, un solo sueño, seguir soñando. Soñar con la libertad, soñar con la justicia, soñar con la igualdad y ojalá ya no tuviera la necesidad de soñarlas.'
+        tarjeta(frase)
+    }else{
+        if(valor=='eleccion2'){
+            let frase='Mi partido es la justicia social; mi bandera la igualdad y tengo como única meta la felicidad de todos.'
+            tarjeta(frase)
+        }else{
+            if(valor=='eleccion3'){
+                let frase='La justicia social no puede ser conseguida por la violencia. La violencia mata lo que intenta crear.'
+                tarjeta(frase)
+            }else{
+                let frase='Saber lo que es justo y no hacerlo es la peor de las cobardías.'
+                tarjeta(frase)
+            }
+        }
+    }
+}
+function leerCookie(){
+    let name="eleccion="
+    let ca=document.cookie.split(';')
+    for(let i=0;i<ca.length;i++){
+        let c=ca[i]
+        while(c.charAt(0)==' '){
+            c=c.substring(1)
+        }
+        if(c.indexOf('eleccion')==0){
+            return c.substring(name.length,c.length)
+        }
+    }
+    return ''
+}
+function tarjeta(frase){
+    console.log(frase);
+}
