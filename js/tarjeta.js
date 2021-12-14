b768db074b2c6131fe9410381f5504c5d09c4b20
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-12-14 11:43:40 +0100

    CSS y JS en proceso

--- js/tarjeta.js
+++ js/tarjeta.js
@@ -23,4 +23,79 @@ function iniciar(){
         //Hacemos el efecto scroll y restamos algunos pixeles
         $('html, body').animate({scrollTop: (position.top - 80)}, 0);
     })
+    let e1=document.getElementById('eleccion1')
+    e1.onclick= eleccion1
+    let e2=document.getElementById('eleccion2')
+    e2.onclick=eleccion2
+    let e3=document.getElementById('eleccion3')
+    e3.onclick=eleccion3
+    let e4=document.getElementById('eleccion4')
+    e4.onclick=eleccion4
+}
+/**
+ * Función que recoge el evento de la imagen seleccionada
+ * @param {*} evento 
+ */
+function eleccion1(evento){
+    let e1=document.getElementById('eleccion1')
+    let cvalor='eleccion1'
+    console.log('eleccion1');
+    crearCookie(cvalor)
+    señalar(e1)
+}
+/**
+ * Función que recoge el evento de la imagen seleccionada
+ * @param {*} evento 
+ */
+function eleccion2(evento){
+    let cvalor='eleccion2'
+    console.log('eleccion2');
+    crearCookie(cvalor)
+    let e2=document.getElementById('eleccion2')
+    señalar(e2)
+}
+/**
+ * Función que recoge el evento de la imagen seleccionada
+ * @param {*} evento 
+ */
+function eleccion3(evento){
+    let cvalor='eleccion3'
+    console.log('eleccion3');
+    crearCookie(cvalor)
+    let e3=document.getElementById('eleccion3')
+    señalar(e3)
+}
+/**
+ * Función que recoge el evento de la imagen seleccionada
+ * @param {*} evento 
+ */
+function eleccion4(evento){
+    let cvalor='eleccion4'
+    console.log('eleccion4');
+    crearCookie(cvalor)
+    let e4=document.getElementById('eleccion4')
+    señalar(e4)
+}
+
+/**
+ * Función crearCookie para guardar el valor de la elección
+ * @param {*} cvalor 
+ */
+function crearCookie(cvalor){
+    let d=new Date()
+    d.setDate(d.getDate()+30)
+    let expires= 'expires='+ d
+    document.cookie= "eleccion="+cvalor+"; "+expires
+}
+/**
+ * Función señalar para resaltar la elección
+ * @param {*} e 
+ */
+function señalar(e){
+    let imagenes = document.getElementsByClassName('imagenesfrases')
+    for (const imagen of imagenes) {
+        console.log(imagen.style.filter.value);
+        imagen.style.filter = 'none'
+    }
+    e.style.filter='drop-shadow(0px 0px 10px #FC716F)'
 }
